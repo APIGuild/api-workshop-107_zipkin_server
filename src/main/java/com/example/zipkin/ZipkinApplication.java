10440d2480bb1db98f3a70f755703a8fbad02faa
KCZhang <kczhang@thoughtworks.com>
2017-11-15 17:12:43 +0800

    delete already code

--- src/main/java/com/example/zipkin/ZipkinApplication.java
+++ src/main/java/com/example/zipkin/ZipkinApplication.java
@@ -2,10 +2,8 @@ package com.example.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
